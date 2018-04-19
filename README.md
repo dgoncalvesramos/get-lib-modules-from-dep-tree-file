c38f394406d5543e2e383f6826bd64e1c20330b5
David Gonçalves Ramos <dram0011@synchronoss.com>
2018-04-19 18:13:39 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# What is it ?
+  Bash script using awk to retrieve modules fetched by a lib in a generated dependency tree file from gradle build tool 
+
+# Usage
+ `$ ./getLibModulesFromDepTree.sh [options] lib_name dep_tree_file`    
+  
+  Options:  
+  `-v` Print the version of the modules fetched by the lib
---
+++ getLibModulesFromDepTree.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+version=
+
+if [[ $# -eq 3 ]]
+then
+	case "$1" in
+	-v) version=1
+	;;
+	*) echo "Usage ./getSubModules.sh [-v] lib depfile" 
+		  exit 0 
+	;;
+	esac
+	if [ ! -e $3 ]
+	then
+		echo "Depfile $3 does not exist"
+		exit 0
+	fi
+elif [[ $# -eq 2 ]]
+then
+	if [ ! -e $2 ]
+	then 
+		echo "Depfile $2 does not exist"
+		exit 0
+	fi
+else 
+	echo "Usage ./getSubModules.sh [-v] lib depfile"
+	exit 0
+fi
+
+if [ ! -z "$version" ]
+then
+ 	awk -v lib="$2" -v depfile="$3" 'BEGIN { print "List of modules of " lib " lib in dependency tree file " depfile " with the associated versions:";
+					 pattern = "^*"lib"$" 
+					 FS=":";}
+					 $1 ~ pattern { print $2":"$3  }
+					 END {}
+					 ' $3 | sort -u
+else
+	awk -v lib="$1" -v depfile="$2" 'BEGIN { print "List of modules of " lib " lib in dependency tree file " depfile " :";
+                                         pattern = "^*"lib"$"
+                                         FS=":";}
+                                         $1 ~ pattern { print $2 }
+                                         END {}
+                                         ' $2 | sort -u
+fi
